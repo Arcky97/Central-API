f03608e104f2f263b5997af55e9b7ddfb93c9e71
Arcky97 <syntixis@gmail.com>
2026-05-24 20:04:19 +0200

    return record forgotten

--- src/services/project-updates.service.ts
+++ src/services/project-updates.service.ts
@@ -1,3 +1,5 @@
+import { API_ERRORS } from "../core/constants/apiErrors";
+import { ApiError } from "../core/errors/ApiError";
 import { ProjectUpdatesRepository } from "../database/repositories/core/ProjectUpdatesRepository";
 
 const projectupdatesRepo = new ProjectUpdatesRepository();
@@ -7,7 +9,13 @@ export class ProjectUpdatesService {
     const record = await projectupdatesRepo.getLatest(limit);
 
     if (!record) {
-      
+      throw new ApiError(
+        404,
+        API_ERRORS.INVALID_INPUT,
+        "Invalid limit input or so"
+      );
     }
+
+    return record;
   }
 }
